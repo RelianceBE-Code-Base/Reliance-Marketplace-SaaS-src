61b61cf7153e7cab7e66360c912a02a8a72caf97
Reliance BE <136088232+RelianceBE-Code-Base@users.noreply.github.com>
2023-12-03 16:56:30 +0100

    Update Upgrade.ps1

--- Deployment/Upgrade.ps1
+++ Deployment/Upgrade.ps1
@@ -26,7 +26,7 @@ Function String-Between
 $ErrorActionPreference = "Stop"
 $WebAppNameAdmin=$WebAppNamePrefix+"-admin"
 $WebAppNamePortal=$WebAppNamePrefix+"-portal"
-$KeyVault=$WebAppNamePrefix+"0"+"-kv"
+$KeyVault=$WebAppNamePrefix+"--kv"
 
 #### THIS SECTION DEPLOYS CODE AND DATABASE CHANGES
 Write-host "#### Deploying new database ####" 
